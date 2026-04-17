c6588c0fc55fd70fcec6076c5d05f95bbf9b2b4a
aman8174-bit <aman73930556@gmail.com>
2026-04-17 13:24:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Data Analytics", "Frontend Developer", "Dashboard Expert"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
